426499ba7e3cdb4462ce6a62d31215679db59a96
Dimitri <dimitri.suter@edorex.ch>
2020-06-02 12:43:30 +0200

    add test file and double precision method

--- NSDoublePrecisionIssue/DoublePrecision.m
+++ NSDoublePrecisionIssue/DoublePrecision.m
@@ -9,3 +9,24 @@
 #import <Foundation/Foundation.h>
 
 
+@interface DoublePrecisio : NSObject
+- (NSDecimalNumber *)parseDouble:(double)dbl;
+
+@end
+
+
+@implementation DoublePrecision : NSObject
+
+- (NSDecimalNumber *)parseDouble:(double)dbl {
+    int digits = 10;
+    NSDecimalNumber *decimalNumber = (NSDecimalNumber*) [NSDecimalNumber numberWithDouble:dbl];
+    NSDecimalNumberHandler *behavior = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundPlain scale:digits raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
+    return [decimalNumber decimalNumberByRoundingAccordingToBehavior:behavior];
+}
+
+
+@end
+
+
+
+
---
+++ NSDoublePrecisionIssue/DoublePrecisionTest.m
@@ -0,0 +1,39 @@
+//
+//  DoublePrecisionTest.m
+//  NSDoublePrecisionIssueTests
+//
+//  Created by Dimitri Suter on 02/06/2020.
+//  Copyright © 2020 Dimitri Suter. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DoublePrecisionTest : XCTestCase
+
+@end
+
+@implementation DoublePrecisionTest
+
+// Ciao Benji - ich glaube hier kann es weiter gehen: https://developer.apple.com/library/archive/documentation/DeveloperTools/Conceptual/testing_with_xcode/chapters/04-writing_tests.html
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
