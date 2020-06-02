1c228bfa6ad268be1d25e739b6383162379e7533
Dimitri <dimitri.suter@edorex.ch>
2020-06-02 12:13:32 +0200

    created double preciions class

---
+++ NSDoublePrecisionIssue/DoublePrecision.m
@@ -0,0 +1,11 @@
+//
+//  DoublePrecision.m
+//  NSDoublePrecisionIssue
+//
+//  Created by Dimitri Suter on 02/06/2020.
+//  Copyright © 2020 Dimitri Suter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
